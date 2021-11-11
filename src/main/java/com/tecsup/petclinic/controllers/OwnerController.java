aee39144883af53306a0bfeeb9dca64819b6314b
joseph <cconisllajoseph@gmail.com>
2021-11-10 22:02:15 -0500

    OwnerController 50%

--- src/main/java/com/tecsup/petclinic/controllers/OwnerController.java
+++ src/main/java/com/tecsup/petclinic/controllers/OwnerController.java
@@ -1,21 +1,21 @@
 package com.tecsup.petclinic.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tecsup.petclinic.dto.OwnerDTO;
-import com.tecsup.petclinic.dto.PetDTO;
 import com.tecsup.petclinic.entities.Owner;
-import com.tecsup.petclinic.entities.Pet;
 import com.tecsup.petclinic.exception.OwnerNotFoundException;
-import com.tecsup.petclinic.exception.PetNotFoundException;
 import com.tecsup.petclinic.services.OwnerService;
 
 @RestController
@@ -50,5 +50,33 @@ public class OwnerController {
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 		}
 	}
-
+	
+	@PutMapping("/owners/{id}")
+	Owner saveOrUpdate(@RequestBody OwnerDTO newOwner,@PathVariable Long id) {
+		Owner owner = null;
+		try {
+			owner = service.findById(id);
+			owner.setFirstname(newOwner.getFirstname());
+			owner.setLastname(newOwner.getLastname());
+			owner.setAddress(newOwner.getAddress());
+			owner.setCity(newOwner.getCity());
+			owner.setTelephone(newOwner.getTelephone());
+			service.update(owner);
+		} catch (OwnerNotFoundException e) {
+			owner = service.create(owner);
+		}
+		return owner;
+	}
+	
+	@DeleteMapping("/owners/{id}")
+	ResponseEntity<String> delete(@PathVariable Long id){
+		
+		try {
+			service.delete(id);
+			return new ResponseEntity<>(""+ id, HttpStatus.OK);
+		} catch (OwnerNotFoundException e) {
+			return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+		}
+	}
+	
 }
--- src/test/java/com/tecsup/petclinic/controllers/OwnerControllerTest.java
+++ src/test/java/com/tecsup/petclinic/controllers/OwnerControllerTest.java
@@ -45,6 +45,48 @@ public class OwnerControllerTest {
 					//.andExpect(jsonPath("$[212].id", is(ID_LAST)));
 	}
 	
+	@Test
+	public void testFindOwnerOK() throws Exception {
+		
+		int ID_SEARCH = 1;
+		String FIRSTNAME_OWNER = "George";
+		String LASTNAME_OWNER = "Franklin";
+		String ADDRESS_OWNER = "110 W. Liberty St.";
+		String CITY_OWNER = "Madison";
+		String TELEPHONE_OWNER = "6085551023";
+		
+		/*
+		   {
+		    "id": 1,
+		    "firstname": "George",
+		    "lastname": "Franklin",
+		    "address": "110 W. Liberty St.",
+		    "city": "Madison",
+		    "telephone": "6085551023"
+  			}
+		 */
+		
+		mockMvc.perform(get("/owners/"+ ID_SEARCH))
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id", is(1)))
+			.andExpect(jsonPath("$.firstname", is(FIRSTNAME_OWNER)))
+			.andExpect(jsonPath("$.lastname", is(LASTNAME_OWNER)))
+			.andExpect(jsonPath("$.address", is(ADDRESS_OWNER)))
+			.andExpect(jsonPath("$.city", is(CITY_OWNER)))
+			.andExpect(jsonPath("$.telephone", is(TELEPHONE_OWNER)));
+		
+	}
 	
+	@Test
+	public void testFindOwnerKO() throws Exception {
+		
+		int ID_SEARCH = 666;
+		
+		mockMvc.perform(get("/owners/"+ ID_SEARCH))
+			.andExpect(status().isNotFound());
+		
+	}
 
+	
 }
