697863f899d6f4addda4b68a132c428d9e826e8c
MicaelaRojas <67761871+MicaelaRojas@users.noreply.github.com>
2021-11-11 12:27:49 -0500

    noveno commit

--- C24B-PETCLINIC/src/test/java/com/tecsup/petclinic/controllers/OwnerControllerTest.java
+++ C24B-PETCLINIC/src/test/java/com/tecsup/petclinic/controllers/OwnerControllerTest.java
@@ -52,6 +52,101 @@ public class OwnerControllerTest {
 					//.andExpect(jsonPath("$[212].id", is(ID_LAST)));
 	}
 	
+	@Test
+	public void testFindOwnerOK() throws Exception {
+		
+		int ID_SEARCH = 1;
+		String FIRSTNAME_OWNER = "George";
+		String LASTNAME_OWNER = "Franklin";
+		String ADDRESS_OWNER = "110 W. Liberty St.";
+		String CITY_OWNER = "Madison";
+		String TELEPHONE_OWNER = "6085551023";
+		
+		/*
+		   {
+		    "id": 1,
+		    "firstname": "George",
+		    "lastname": "Franklin",
+		    "address": "110 W. Liberty St.",
+		    "city": "Madison",
+		    "telephone": "6085551023"
+  			}
+		 */
+		
+		mockMvc.perform(get("/owners/"+ ID_SEARCH))
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id", is(1)))
+			.andExpect(jsonPath("$.firstname", is(FIRSTNAME_OWNER)))
+			.andExpect(jsonPath("$.lastname", is(LASTNAME_OWNER)))
+			.andExpect(jsonPath("$.address", is(ADDRESS_OWNER)))
+			.andExpect(jsonPath("$.city", is(CITY_OWNER)))
+			.andExpect(jsonPath("$.telephone", is(TELEPHONE_OWNER)));
+		
+	}
 	
+	@Test
+	public void testFindOwnerKO() throws Exception {
+		
+		int ID_SEARCH = 666;
+		
+		mockMvc.perform(get("/owners/"+ ID_SEARCH))
+			.andExpect(status().isNotFound());
+		
+	}
+	
+	@Test
+	public void testCreateOwner() throws Exception {
+		
+		String FIRSTNAME_OWNER = "Cristhian";
+		String LASTNAME_OWNER = "Lope";
+		String ADDRESS_OWNER = "110 W. Liberty St.";
+		String CITY_OWNER = "Madison";
+		String TELEPHONE_OWNER = "6182551023";
+		
+		OwnerDTO newOwner = new OwnerDTO(FIRSTNAME_OWNER,LASTNAME_OWNER,ADDRESS_OWNER,CITY_OWNER,TELEPHONE_OWNER);
+		
+		logger.info(newOwner.toString());
+		logger.info(om.writeValueAsString(newOwner));
+		
+		mockMvc.perform(post("/owners")
+				.content(om.writeValueAsString(newOwner))
+				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.firstname", is(FIRSTNAME_OWNER)))
+				.andExpect(jsonPath("$.lastname", is(LASTNAME_OWNER)))
+				.andExpect(jsonPath("$.address", is(ADDRESS_OWNER)))
+				.andExpect(jsonPath("$.city", is(CITY_OWNER)))
+				.andExpect(jsonPath("$.telephone", is(TELEPHONE_OWNER)));
+				
+		
+	}
+	
+	@Test
+	public void testDeleteOwner() throws Exception {
+		
+		String FIRSTNAME_OWNER = "George";
+		String LASTNAME_OWNER = "Franklin";
+		String ADDRESS_OWNER = "110 W. Liberty St.";
+		String CITY_OWNER = "Madison";
+		String TELEPHONE_OWNER = "6085551023";
+		
+		OwnerDTO newOwner = new OwnerDTO(FIRSTNAME_OWNER,LASTNAME_OWNER,ADDRESS_OWNER,CITY_OWNER,TELEPHONE_OWNER);
+		
+		ResultActions mvcActions = mockMvc.perform(post("/owners")
+				.content(om.writeValueAsString(newOwner))
+				.header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isCreated());
+
+		String response = mvcActions.andReturn().getResponse().getContentAsString();
+		
+		Integer id = JsonPath.parse(response).read("$.id");
+		
+		mockMvc.perform(delete("/owners/" + id))
+				.andExpect(status().isOk());
+		
+	}
 	 
 }
